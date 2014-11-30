2da82f90ff4ce0553f62ca4194453131c27cbd10
Cody Foltz <Codablo@Gmail.com>
2014-11-29 21:17:51 -0700

    Starting Bridge Connection
    Add -ObjC flag to linker, Experimented with HueSDK, starting UI for
    connection.

--- iPhone/Prism Lights/Prism Lights/AppDelegate.swift
+++ iPhone/Prism Lights/Prism Lights/AppDelegate.swift
@@ -12,15 +12,39 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    var hueSDK :PHHueSDK?
+    var x :PHNotificationManager?
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-//        var boo = PHHueSDK()
-//        boo.enableLogging(true)
         
+        hueSDK = PHHueSDK()
+        hueSDK!.enableLogging(true)
+        hueSDK!.startUpSDK()
+        hueSDK!.setBridgeToUseWithIpAddress("50.168.199.108", macAddress: "00:17:88:0A:6D:13")
+        x = PHNotificationManager.defaultManager()
+        x!.registerObject(self, withSelector: "woot", forNotification: "LOCAL_CONNECTION_NOTIFICATION")
+        
+        x!.registerObject(self, withSelector: "woot2", forNotification: "NO_LOCAL_CONNECTION_NOTIFICATION")
+        
+        x!.registerObject(self, withSelector: "woot3", forNotification: "NO_LOCAL_AUTHENTICATION_NOTIFICATION")
+        
+        hueSDK!.enableLocalConnection()
         // Override point for customization after application launch.
         return true
     }
+    func woot(){
+        println("WOOT")
+    }
+    
+    func woot2(){
+        println("#*#*#*#*#*##* Number 2")
+    }
+    
+    func woot3(){
+        println("*#*#*#*#*#* Number 3")
+    }
+    
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
