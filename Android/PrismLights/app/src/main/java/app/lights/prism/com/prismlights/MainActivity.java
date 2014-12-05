434169ee8e26ca97a66a9033af894a33a0def056
Trudy Firestone <trudyfirestone@gmail.com>
2014-12-05 13:25:35 -0100

    Searching causes the android application to freeze.

--- Android/PrismLights/app/src/main/java/app/lights/prism/com/prismlights/MainActivity.java
+++ Android/PrismLights/app/src/main/java/app/lights/prism/com/prismlights/MainActivity.java
@@ -4,50 +4,111 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTabHost;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.brian_oh.prismlights.R;
+import com.philips.lighting.hue.sdk.PHAccessPoint;
+import com.philips.lighting.hue.sdk.PHBridgeSearchManager;
+import com.philips.lighting.hue.sdk.PHHueSDK;
+import com.philips.lighting.hue.sdk.PHMessageType;
+import com.philips.lighting.hue.sdk.PHSDKListener;
+import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHHueParsingError;
+
+import java.util.List;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements PHSDKListener{
+
+    PHHueSDK hueBridgeSdk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FragmentTabHost tabHost = new FragmentTabHost(this);
         setContentView(R.layout.activity_main);
-        Dialog dialog = new ProgressDialog(this);
-        dialog.show();
-        TextView textView = new TextView(this);
-        textView.setText("Searching For Bridge...");
-        textView.setTextColor(Color.WHITE);
-        dialog.setContentView(textView);
+
+        hueBridgeSdk = PHHueSDK.getInstance();
+        hueBridgeSdk.setAppName("Prism Lights");
+        hueBridgeSdk.setDeviceName(Build.MODEL);
+        hueBridgeSdk.getNotificationManager().registerSDKListener(this);
+//        Dialog dialog = new ProgressDialog(this);
+//        dialog.show();
+//        TextView textView = new TextView(this);
+//        textView.setText("Searching For Bridge...");
+//        textView.setTextColor(Color.WHITE);
+//        dialog.setContentView(textView);
+        PHBridgeSearchManager bridgeSearchManager = (PHBridgeSearchManager) hueBridgeSdk.getSDKService(PHHueSDK.SEARCH_BRIDGE);
+        bridgeSearchManager.search(true,true);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onCacheUpdated(List<Integer> cacheNotificationsList, PHBridge bridge) {
+        if (cacheNotificationsList.contains(PHMessageType.LIGHTS_CACHE_UPDATED)) {
+            System.out.println("Lights Cache Updated ");
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    /**
+     * From API:
+     * Here it is recommended to set your connected bridge in your sdk object (as above) and start the heartbeat.
+     * At this point you are connected to a bridge so you should pass control to your main program/activity.
+     * Also it is recommended you store the connected IP Address/ Username in your app here.  This will allow easy automatic connection on subsequent use.
+     */
+    public void onBridgeConnected(PHBridge phBridge) {
+        hueBridgeSdk.setSelectedBridge(phBridge);
+        hueBridgeSdk.enableHeartbeat(phBridge, PHHueSDK.HB_INTERVAL);
+    }
+
+    @Override
+    /**
+     * From API:
+     * Arriving here indicates that Pushlinking is required (to prove the User has physical access to the bridge).  Typically here
+     * you will display a pushlink image (with a timer) indicating to to the user they need to push the button on their bridge within 30 seconds.
+     */
+    public void onAuthenticationRequired(PHAccessPoint accessPoint) {
+        hueBridgeSdk.startPushlinkAuthentication(accessPoint);
+    }
+
+    @Override
+    /**
+     * From API:
+     * Handle your bridge search results here.  Typically if multiple results are returned you will want to display them in a list
+     * and let the user select their bridge.   If one is found you may opt to connect automatically to that bridge.
+     */
+    public void onAccessPointsFound(List<PHAccessPoint> accessPoints) {
+        Toast toast = Toast.makeText(this, "Access Points Found", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    /**
+     * From API:
+     * Here you can handle events such as Bridge Not Responding, Authentication Failed and Bridge Not Found
+     */
+    public void onError(int i, String s) {
+        Toast toast = Toast.makeText(this, "Error", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void onConnectionResumed(PHBridge bridge) {
+
+    }
+
+    @Override
+    /**
+     * From API:
+     * Here you would handle the loss of connection to your bridge.
+     */
+    public void onConnectionLost(PHAccessPoint phAccessPoint) {
+
+    }
+
+    @Override
+    public void onParsingErrors(List<PHHueParsingError> phHueParsingErrors) {
+
     }
 }
---
+++ Android/PrismLights/app/src/main/java/app/lights/prism/com/prismlights/SettingsFragment.java
@@ -0,0 +1,70 @@
+package app.lights.prism.com.prismlights;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SettingsFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class SettingsFragment extends Fragment {
+
+//    private OnFragmentInteractionListener interactionListener;
+
+    public SettingsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.settings_fragment, container);
+    }
+//
+//
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            interactionListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+////            throw new ClassCastException(activity.toString()
+////                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        interactionListener = null;
+//    }
+//
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p/>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        public void onFragmentInteraction(Uri uri);
+//    }
+
+}
