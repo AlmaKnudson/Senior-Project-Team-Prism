ebef3167d8b9831387738c68df308e9c08a1cfc8
Cody Foltz <Codablo@Gmail.com>
2014-12-10 09:44:59 -0700

    Added Cells for collection in Home screen
    Added Cells for collection in Home screen.

--- iPhone/Prism Lights/Prism Lights/HomeController.swift
+++ iPhone/Prism Lights/Prism Lights/HomeController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  HomeController.swift
 //  Prism Lights
 //
 //  Created by Cody Foltz on 11/19/14.
@@ -10,38 +10,61 @@ import UIKit
 let MAX_HUE:UInt32 = 65535
 
 class HomeController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var bulbCollectionView: UICollectionView!
     
+    
+    var cache :PHBridgeResourcesCache? = nil;
+    var lightCount :Int = 0;
     
     //MARK: - UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
     
-    override func viewDidAppear(animated: Bool) {
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
         var manager = PHNotificationManager.defaultManager()
         manager!.registerObject(self, withSelector: "HeartBeatReceived", forNotification: "LOCAL_CONNECTION_NOTIFICATION")
         manager!.registerObject(self, withSelector: "NetworkConnectionLost", forNotification: "NO_LOCAL_CONNECTION_NOTIFICATION")
         manager!.registerObject(self, withSelector: "NotAuthorized", forNotification: "NO_LOCAL_AUTHENTICATION_NOTIFICATION")
         
+        
+        //Check that we are connected to bridge.
         if !((UIApplication.sharedApplication().delegate as AppDelegate).hueSDK!.localConnected()){
-            //TODO: Popup notification telling user that there is no connection yet...
+            //Connect to bridge
+            (UIApplication.sharedApplication().delegate as AppDelegate).hueSDK!.enableLocalConnection()
         } else{
-            //TODO: Grab list of bulbs/lights
-            var cache = PHBridgeResourcesReader.readBridgeResourcesCache()
-            for light in cache.lights.values{
-                var lightState = PHLightState()
-                
-                lightState.hue = 65535
-                lightState.brightness = 150
-                lightState.saturation = 200
-                
-                var bridgeSendAPI = PHBridgeSendAPI()
-                bridgeSendAPI.updateLightStateForId(light.identifier, withLightState: lightState, completionHandler: nil)
-            }
-            
+            cache = PHBridgeResourcesReader.readBridgeResourcesCache()
+            lightCount = (cache?.lights.count)!
         }
+    }
+    
+    
+    /**
+    Lorem ipsum dolor sit amet.
+    
+    :param: bar Consectetur adipisicing elit.
+    
+    :returns: Sed do eiusmod tempor.
+    */ 
+    override func viewDidAppear(animated: Bool) {
+        
+        
+        
         
     }
     
@@ -58,17 +81,20 @@ class HomeController: UIViewController, UICollectionViewDataSource, UICollection
     //MARK: - UICollectionView Methods
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
-        
-        //TODO: Return the correct number of light bulbs
-        return 0;
+        return lightCount;
     }
     
     // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
         
+        var cell = bulbCollectionView.dequeueReusableCellWithReuseIdentifier("bulb", forIndexPath: indexPath) as? BulbCollectionCell
+        if( cell == nil){
+            cell = BulbCollectionCell()
+        }
+        cell!.SetBulbLabel("asjfdas")
         
         //TODO: Return a correct cell for the view
-        return BulbCollectionCell()
+        return cell!
     }
     
     //TODO: Handle taps and long presses
@@ -77,7 +103,10 @@ class HomeController: UIViewController, UICollectionViewDataSource, UICollection
     //MARK: Notification Methods
     
     func HeartBeatReceived(){
-        //TODO: Update UI based on new info in the cache
+        cache = PHBridgeResourcesReader.readBridgeResourcesCache()
+        lightCount = (cache?.lights.count)!
+                //TODO: Update UI based on new info in the cache.
+        self.bulbCollectionView.reloadData()
     }
     
     func NetworkConnectionLost(){
@@ -92,6 +121,22 @@ class HomeController: UIViewController, UICollectionViewDataSource, UICollection
         hueSDK.disableLocalConnection()
         //TODO: Notify user of lost Authorization
     }
+    
+    
+    
+    /*
+    
+    for light in cache!.lights.values{
+    var lightState = PHLightState()
+    
+    lightState.hue = 65535
+    lightState.brightness = 150
+    lightState.saturation = 200
+    
+    var bridgeSendAPI = PHBridgeSendAPI()
+    bridgeSendAPI.updateLightStateForId(light.identifier, withLightState: lightState, completionHandler: nil)
+    }
+    */
 
 
 }
