9313e8d21252ba3d69a42d46ab228ead864fe2a8
Alma Knudson <knudson.dell@gmail.com>
2015-02-19 21:31:30 -0700

    Still working on waveform gobblygoop.

---
+++ iPhone/Prism Lights/Prism Lights/Header.h
@@ -0,0 +1,44 @@
+//
+//  MusicController.h
+//  Prism Lights
+//
+//  Created by Alma Knudson on 2/19/15.
+//  Copyright (c) 2015 Prism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ EZAudio
+ */
+#import "EZAudio.h"
+
+/**
+ Accelerate
+ */
+#import <Accelerate/Accelerate.h>
+
+
+
+/**
+ The FFTViewController demonstrates how to use the Accelerate framework to calculate the real-time FFT of audio data provided by an EZAudioMicrophone.
+ */
+@interface MusicController : UIViewController <EZMicrophoneDelegate>
+
+#pragma mark - Components
+/**
+ EZAudioPlot for frequency plot
+ */
+@property (nonatomic,weak) IBOutlet EZAudioPlot *audioPlotFreq;
+
+/**
+ EZAudioPlot for time plot
+ */
+@property (nonatomic,weak) IBOutlet EZAudioPlotGL *audioPlotTime;
+
+/**
+ Microphone
+ */
+@property (nonatomic,strong) EZMicrophone *microphone;
+
+@end
