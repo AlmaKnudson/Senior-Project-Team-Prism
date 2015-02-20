f416d9e8a9026285ae21929c665fa09bb8f2a2bd
Alma Knudson <knudson.dell@gmail.com>
2015-02-19 19:35:30 -0700

    EZAudio

---
+++ iPhone/Prism Lights/Prism Lights/SignalProcessing.h
@@ -0,0 +1,13 @@
+//
+//  SignalProcessing.h
+//  Prism Lights
+//
+//  Created by Alma Knudson on 2/19/15.
+//  Copyright (c) 2015 Prism. All rights reserved.
+//
+
+#ifndef Prism_Lights_SignalProcessing_h
+#define Prism_Lights_SignalProcessing_h
+
+
+#endif
---
+++ iPhone/Prism Lights/Prism Lights/test.m
@@ -0,0 +1,9 @@
+//
+//  test.m
+//  Prism Lights
+//
+//  Created by Alma Knudson on 2/19/15.
+//  Copyright (c) 2015 Prism. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
