10f12e459b0d668d40940996ae392b618f0c8ec0
Alma Knudson <knudson.dell@gmail.com>
2015-02-21 21:39:43 -0700

    Music Waveform iOS

--- iPhone/Prism Lights/MusicController.m
+++ iPhone/Prism Lights/MusicController.m
@@ -23,23 +23,39 @@
 @synthesize audioPlotTime;
 @synthesize microphone;
 
+
+//Counter
+int counter;
+float min = 500;
+float max = 500;
+int currentRangeBin;
+float runningMinSound = 0;
+float runningMaxSound = 0;
+
+bool sendChange;
+
 #pragma mark - Customize the Audio Plot
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
+    
+    counter = 0;
+    sendChange = false;
+    
+    
     /*
      Customizing the audio plot's look
      */
     // Setup time domain audio plot
-    self.audioPlotTime.backgroundColor = [UIColor colorWithRed: 0.569 green: 0.82 blue: 0.478 alpha: 1];
+    self.audioPlotTime.backgroundColor = [UIColor colorWithRed: 0.0904 green: 0.0901 blue: 0.105 alpha: 1];
     self.audioPlotTime.color           = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
     self.audioPlotTime.shouldFill      = YES;
     self.audioPlotTime.shouldMirror    = YES;
     self.audioPlotTime.plotType        = EZPlotTypeRolling;
     
     // Setup frequency domain audio plot
-    self.audioPlotFreq.backgroundColor = [UIColor colorWithRed: 0.984 green: 0.471 blue: 0.525 alpha: 1];
+    self.audioPlotFreq.backgroundColor = [UIColor colorWithRed: 0.114 green: 0.111 blue: 0.125 alpha: 1];
     self.audioPlotFreq.color           = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
     self.audioPlotFreq.shouldFill      = YES;
     self.audioPlotFreq.plotType        = EZPlotTypeBuffer;
@@ -82,6 +98,9 @@
 }
 
 -(void)updateFFTWithBufferSize:(float)bufferSize withAudioData:(float*)data {
+    if(counter > 4){
+        counter = 0;
+        
     
     // For an FFT, numSamples must be a power of 2, i.e. is always even
     int nOver2 = bufferSize/2;
@@ -109,10 +128,85 @@
         // Bind the value to be less than 1.0 to fit in the graph
         amp[i] = [EZAudio MAP:mag leftMin:0.0 leftMax:maxMag rightMin:0.0 rightMax:1.0];
     }
-    
+    float maxSound = data[0];
+    for(int i = 0; i < bufferSize; i ++){
+        maxSound = maxSound > data[i] ? maxSound  : data[i];
+        runningMaxSound = runningMaxSound > maxSound ? (runningMaxSound - .00005)  : maxSound;
+    }
+        /* Play with the frequency-->Determine high/mid/low*/
+        if(maxMag > max){
+            max = maxMag;
+        } else{
+            max = max - 200;
+        }
+        if(maxMag < min){
+            min = maxMag;
+        } else {
+            min = min + 20;
+        }
+        // NSLog(@"%f \t %f", max, min);
+        /* Plotting the time domain plot takes around 50-60% CPU... HOLY MOSES */
+        
+        if(maxMag > min && maxMag < (max / 5)){
+            currentRangeBin = 1;
+            //            NSLog(@"LOWS");
+        } else if(maxMag > (max / 5) && maxMag < (max - (max / 5))){
+            currentRangeBin = 2;
+            //            NSLog(@"MID");
+        } else if(maxMag > (max - (max / 5))){
+            currentRangeBin = 3;
+            //            NSLog(@"HIGHS");
+        }
+        
+        
+        /* Play with the sound-->Determine whether or not to CHANGE bulbs */
+        if(maxSound < 0)
+            maxSound = maxSound * -1;
+        if( (runningMaxSound / maxSound) < 2 ){
+            //I think this is LOUD-->FORTISSIMO
+//            NSLog(@"HARD HIT");
+            if(sendChange){
+                if(currentRangeBin == 1){
+                    //SEND CHANGE TO BIN 1
+                    NSLog(@"HIGH");
+                } else if(currentRangeBin == 2) {
+                    //SEND CHANGE TO BIN 2
+                     NSLog(@"MID");
+                } else if(currentRangeBin == 3){
+                    //SNED CHANGE TO BIN 3
+                     NSLog(@"LOW");
+                }
+                
+                sendChange = false;
+            }
+        } else if( (runningMaxSound / maxSound) < 4 ){
+            //This is medium loudness. meto forte
+//            NSLog(@"MEDIUM HIT");
+            sendChange = true;
+        } else if( (runningMaxSound / maxSound) < 6 ){
+            //This is quiter.
+//            NSLog(@"SOFT HIT");
+            sendChange = true;
+        } else {
+            //This is too quite to do anything about.
+        }
+       // NSLog(@"%f", maxSound);
+        
+        /* Plotting frequency domain + FFT --> 3% CPU and minimal memory footprint */
     // Update the frequency domain plot
     [self.audioPlotFreq updateBuffer:amp
                       withBufferSize:nOver2];
+
+        
+        // Update time domain plot
+        [self.audioPlotTime updateBuffer:data
+                          withBufferSize:bufferSize];
+        
+        
+        
+    } else {
+        counter = counter + 1;
+    }
     
     
     //We have the FFT results of the current audio data buffer:
