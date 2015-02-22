06a83e7e30bb2a4f8fb5c2fa96b605e454cbb0d4
Alma Knudson <knudson.dell@gmail.com>
2015-02-22 00:13:52 -0700

    Music visualization (FFT + mic input + Visualization)

--- Android/PrismLights/app/src/main/java/app/lights/prism/com/prismlights/MusicFragment.java
+++ Android/PrismLights/app/src/main/java/app/lights/prism/com/prismlights/MusicFragment.java
@@ -8,6 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Equalizer;
+import android.media.audiofx.Visualizer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
 import app.lights.prism.com.prismlights.R;
 
 /**
@@ -27,6 +49,18 @@ public class MusicFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private static final String TAG = "AudioFxDemo";
+
+    private static final float VISUALIZER_HEIGHT_DIP = 50f;
+
+    private MediaPlayer mMediaPlayer;
+    private Visualizer mVisualizer;
+    private Equalizer mEqualizer;
+
+    private LinearLayout mLinearLayout;
+    private VisualizerView mVisualizerView;
+    private TextView mStatusTextView;
 
     private OnFragmentInteractionListener mListener;
 
@@ -55,6 +89,40 @@ public class MusicFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        mStatusTextView = new TextView(getActivity());
+        mLinearLayout = new LinearLayout(getActivity());
+        mLinearLayout.setOrientation(LinearLayout.VERTICAL);
+        mLinearLayout.addView(mStatusTextView);
+
+        getActivity().setContentView(mLinearLayout);
+
+        //Create the MediaPlayer
+        //WTF should I do instead of R.id.micButton-->
+        mMediaPlayer = MediaPlayer.create(getActivity(), R.id.micButton);
+        //Log some shiznit-->
+        Log.d(TAG, "MediaPlayer audio session ID: " + mMediaPlayer.getAudioSessionId());
+
+        //Call auxiliary functions::
+        setupVisualizerFxAndUI();
+        setupEqualizerFxAndUI();
+
+        mVisualizer.setEnabled(true);
+
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mVisualizer.setEnabled(false);
+            }
+        });
+
+        mMediaPlayer.start();
+        mStatusTextView.setText("PLAYING AUDIO...");
+
+
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -107,4 +175,154 @@ public class MusicFragment extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
+
+    private void setupVisualizerFxAndUI() {
+        // Create a VisualizerView (defined below), which will render the simplified audio
+        // wave form to a Canvas.
+        mVisualizerView = new VisualizerView(getActivity());
+        mVisualizerView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT,
+                (int)(VISUALIZER_HEIGHT_DIP * getResources().getDisplayMetrics().density)));
+        mLinearLayout.addView(mVisualizerView);
+
+        // Create the Visualizer object and attach it to our media player.
+        mVisualizer = new Visualizer(mMediaPlayer.getAudioSessionId());
+        mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+        mVisualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
+            public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes,
+                                              int samplingRate) {
+                mVisualizerView.updateVisualizer(bytes);
+            }
+
+            public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int samplingRate) {}
+        }, Visualizer.getMaxCaptureRate() / 2, true, false);
+    }
+
+    private void setupEqualizerFxAndUI() {
+        // Create the Equalizer object (an AudioEffect subclass) and attach it to our media player,
+        // with a default priority (0).
+        mEqualizer = new Equalizer(0, mMediaPlayer.getAudioSessionId());
+        mEqualizer.setEnabled(true);
+
+        TextView eqTextView = new TextView(getActivity());
+        eqTextView.setText("Equalizer:");
+        mLinearLayout.addView(eqTextView);
+
+        short bands = mEqualizer.getNumberOfBands();
+
+        final short minEQLevel = mEqualizer.getBandLevelRange()[0];
+        final short maxEQLevel = mEqualizer.getBandLevelRange()[1];
+
+        for (short i = 0; i < bands; i++) {
+            final short band = i;
+
+            TextView freqTextView = new TextView(getActivity());
+            freqTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            freqTextView.setGravity(Gravity.CENTER_HORIZONTAL);
+            freqTextView.setText((mEqualizer.getCenterFreq(band) / 1000) + " Hz");
+            mLinearLayout.addView(freqTextView);
+
+            LinearLayout row = new LinearLayout(getActivity());
+            row.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView minDbTextView = new TextView(getActivity());
+            minDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            minDbTextView.setText((minEQLevel / 100) + " dB");
+
+            TextView maxDbTextView = new TextView(getActivity());
+            maxDbTextView.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            maxDbTextView.setText((maxEQLevel / 100) + " dB");
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.weight = 1;
+            SeekBar bar = new SeekBar(getActivity());
+            bar.setLayoutParams(layoutParams);
+            bar.setMax(maxEQLevel - minEQLevel);
+            bar.setProgress(mEqualizer.getBandLevel(band));
+
+            bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser) {
+                    mEqualizer.setBandLevel(band, (short) (progress + minEQLevel));
+                }
+
+                public void onStartTrackingTouch(SeekBar seekBar) {}
+                public void onStopTrackingTouch(SeekBar seekBar) {}
+            });
+
+            row.addView(minDbTextView);
+            row.addView(bar);
+            row.addView(maxDbTextView);
+
+            mLinearLayout.addView(row);
+        }
+    }
+
+
+
+
+}
+
+/**
+ * A simple class that draws waveform data received from a
+ * {@link Visualizer.OnDataCaptureListener#onWaveFormDataCapture }
+ */
+class VisualizerView extends View {
+    private byte[] mBytes;
+    private float[] mPoints;
+    private Rect mRect = new Rect();
+
+    private Paint mForePaint = new Paint();
+
+    public VisualizerView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        mBytes = null;
+
+        mForePaint.setStrokeWidth(1f);
+        mForePaint.setAntiAlias(true);
+        mForePaint.setColor(Color.rgb(0, 128, 255));
+    }
+
+    public void updateVisualizer(byte[] bytes) {
+        mBytes = bytes;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (mBytes == null) {
+            return;
+        }
+
+        if (mPoints == null || mPoints.length < mBytes.length * 4) {
+            mPoints = new float[mBytes.length * 4];
+        }
+
+        mRect.set(0, 0, getWidth(), getHeight());
+
+        for (int i = 0; i < mBytes.length - 1; i++) {
+            mPoints[i * 4] = mRect.width() * i / (mBytes.length - 1);
+            mPoints[i * 4 + 1] = mRect.height() / 2
+                    + ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
+            mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
+            mPoints[i * 4 + 3] = mRect.height() / 2
+                    + ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2) / 128;
+        }
+
+        canvas.drawLines(mPoints, mForePaint);
+    }
 }
