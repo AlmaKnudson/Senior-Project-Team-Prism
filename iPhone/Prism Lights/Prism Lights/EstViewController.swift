13036ae785b312c870379966cc6c357aa995b20b
Alma Knudson <knudson.dell@gmail.com>
2015-03-05 20:58:47 -0700

    Adding EST

---
+++ iPhone/Prism Lights/Prism Lights/EstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  EstViewController.swift
+//  Prism Lights
+//
+//  Created by Alma Knudson on 3/5/15.
+//  Copyright (c) 2015 Prism. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class EstViewController:UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad();
+        //TODO--Additional setup
+    }
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning();
+        //Dispose of any resources that can be recreated.
+    }
+}
