d08b8067f38ae6725a7c702115d2630325562834
Trudy Firestone <trudyfirestone@gmail.com>
2015-04-15 17:30:42 -0600

    Added styles and removed all switches

--- Android/PrismLights/app/src/main/java/app/lights/prism/com/prismlights/LightSettingsFragment.java
+++ Android/PrismLights/app/src/main/java/app/lights/prism/com/prismlights/LightSettingsFragment.java
@@ -9,11 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.SeekBar;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.philips.lighting.hue.sdk.PHHueSDK;
